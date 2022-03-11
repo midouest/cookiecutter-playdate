de3bb608b84635d4fd034872ba07857331450287
Robin <midouesterner@gmail.com>
2022-03-11 17:58:33 -0600

    generated game draws greeting text with project name

--- {{cookiecutter.directory_name}}/source/main.lua
+++ {{cookiecutter.directory_name}}/source/main.lua
@@ -6,11 +6,18 @@ import "CoreLibs/timer"
 local pd<const> = playdate
 local gfx<const> = pd.graphics
 
-function setupGame() end
+function setupGame()
+    font = gfx.getFont()
+    greeting = "Hello, {{cookiecutter.name}}!"
+end
 
 setupGame()
 
 function pd.update()
+    local w = font:getTextWidth(greeting)
+    local h = font:getHeight()
+    gfx.drawText(greeting, (400 - w) / 2, (240 - h) / 2)
+
     gfx.sprite.update()
     pd.timer.updateTimers()
     pd.drawFPS(0, 0)
